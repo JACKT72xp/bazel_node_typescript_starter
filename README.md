e0d36cf4a39781730375009c626bff8cccf09b98
Jack Torpoco <jackt72xp@gmail.com>
2024-03-26 01:35:52 +0100

    Bazel Node Typescript Starter: First Push

--- README.md
+++
@@ -1,2 +0,0 @@
-# bazel_node_typescript_starter
-Bazel + Typescript , Starte repository to build and transpile code + ibazel
--- bazel-out/darwin_arm64-fastbuild/bin/common/index.test.ts
+++
@@ -1,5 +0,0 @@
-import {number} from './index'
-
-test("number should be the answer to everything", () => {
-  expect(number).toEqual(42);
-});
